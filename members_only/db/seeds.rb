d6e289782b08f0ddd985d9092674a90ff9dc2cd3
pip36 <phil_23@live.co.uk>
2017-07-09 11:23:50 +0100

    added basic styling, and seed data

--- members_only/db/seeds.rb
+++ members_only/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  u = User.create(name: Faker::Name.name, email: "example#{i}@example.com", password: "password", password_confirmation: "password")
+end
+
+User.all.each do |u|
+  3.times do
+    u.posts.create(name: Faker::HarryPotter.location, body: Faker::HarryPotter.quote)
+  end
+end
